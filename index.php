b49b2d150a5fb0030454278cfb64dee7044428e6
omchauhansa29 <omchauhansa29@gmail.com>
2014-03-09 03:23:22 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,147 @@
+ 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<script src="js/jquery-1.js" type="text/javascript"></script>
+<script src="js/jquery-ui-1.js" type="text/javascript"></script>
+<script src="js/jQueryRotate.js" type="text/javascript"></script>
+<script src="js/common.js" type="text/javascript"></script>
+<link href="js/jquery-ui-1.css" rel="stylesheet" type="text/css">
+<link href="js/common.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body style="cursor: auto;">
+
+<div class="mine_div">
+ 
+
+<!--middle-->
+<div class="middle_div">
+
+
+ 
+
+<div>
+
+    <div style="width:485px; height:495px; float:left;">
+     
+     
+     <div style="height:474px; border:1px solid #808080;">
+     <table width="100%" height="53" border="0" cellspacing="0" cellpadding="0" style="color:#8c316a;">
+  <tr>
+    <td width="10" align="left" valign="middle">&nbsp;</td>
+    <td align="left" valign="middle">&nbsp;</td>
+    <td width="15" align="left" valign="middle">&nbsp;</td>
+    <td align="left" valign="middle">&nbsp;</td>
+    <td align="left" valign="middle">&nbsp;</td>
+  </tr>
+  
+   
+  
+</table>
+
+   <div style="width:460px; height:365px; margin-left:20px;">  
+     <div id="site">
+     	<div class="top_panel" style="float:left; height:50px;">
+            <a href="#" class="new_img">New</a>
+            <a href="#" class="copy_img">Copy</a>
+            <a href="#" class="remove_img">Remove</a>
+            <a href="#" class="rotateh_img">Horizontal</a>
+            <a href="#" class="rotatev_img">Vertical</a>
+            <a href="#" class="zoomin_img">Zoom In</a>
+            <a href="#" class="zoomout_img">Zoom Out</a>
+        </div>
+       <div id="items">
+            <div class="dress">
+                              
+                           <div class="img_div">
+                                <a href="#foo" title="Red Valentino Knitted lace mini dress - $795 - net-a-porter.com">
+                                    <img width="101" border="0" height="99" class="draggable-menu ui-draggable" alt="images/1.jpg" src="images/1.jpg">           
+                                </a>
+                            </div>
+               			               
+                           
+               			               
+                           <div class="img_div">
+                                <a href="#foo" title="Red Valentino Knitted lace mini dress - $795 - net-a-porter.com">
+                                    <img width="101" border="0" height="99" class="draggable-menu ui-draggable" alt="images/3.jpg" src="images/3.jpg">           
+                                </a>
+                            </div>
+               			               
+                           <div class="img_div">
+                                <a href="#foo" title="Red Valentino Knitted lace mini dress - $795 - net-a-porter.com">
+                                    <img width="101" border="0" height="99" class="draggable-menu ui-draggable" alt="images/4.jpg" src="images/4.jpg">           
+                                </a>
+                            </div>
+               			               
+                           <div class="img_div">
+                                <a href="#foo" title="Red Valentino Knitted lace mini dress - $795 - net-a-porter.com">
+                                    <img width="101" border="0" height="99" class="draggable-menu ui-draggable" alt="images/5.jpg" src="images/5.jpg">           
+                                </a>
+                            </div>
+               			               
+                           <div class="img_div">
+                                <a href="#foo" title="Red Valentino Knitted lace mini dress - $795 - net-a-porter.com">
+                                    <img width="101" border="0" height="99" class="draggable-menu ui-draggable" alt="images/6.jpg" src="images/6.jpg">           
+                                </a>
+                            </div>
+							
+							 <div class="img_div">
+                                <a href="#foo" title="Red Valentino Knitted lace mini dress - $795 - net-a-porter.com">
+                                    <img width="101" border="0" height="99" class="draggable-menu ui-draggable" alt="images/10.jpg" src="images/10.jpg">           
+                                </a>
+                            </div>
+               			               
+                           <div class="img_div">
+                                <a href="#foo" title="Red Valentino Knitted lace mini dress - $795 - net-a-porter.com">
+                                    <img width="101" border="0" height="99" class="draggable-menu ui-draggable" alt="images/7.jpg" src="images/7.jpg">           
+                                </a>
+                            </div>
+               			               
+                            
+               			               
+                            
+         </div>   
+      </div>
+        
+        
+        
+       
+    
+        
+        
+        <div class="cb"></div>
+    </div>
+  
+  
+  
+  </div> 
+  
+   
+     </div>
+    
+    </div>
+
+<div style="width:440px; height:495px; float:left; margin-left:15px; border:1px solid #808080;">
+    
+        <div class="ui-droppable" id="canvas">
+            <div class="draggable-item" style="position: absolute; top: -12px; left: 482.2px; opacity: 1;"></div>
+       </div>
+</div>
+
+</div>
+
+ 
+
+ 
+</div>
+<!--end middle-->
+ 
+ 
+</div>
+
+</body>
+</html>
